621d5c135a6e987e9bd080938b47660522096251
Lanz <jeffersonlanz.gementiza@devpartners.co>
2021-07-27 16:44:05 +0800

    Secret Parse

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,24 @@
 /// <reference types="cypress" />
 
-// export a function
+// // export a function
+// module.exports = (on, config) => {
+//   // configure plugins here
+// };
+
+// promisified fs module
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve("cypress", "config", `${file}.json`);
+
+  return fs.readJson(pathToConfigFile);
+}
+
+// plugins file
 module.exports = (on, config) => {
-  // configure plugins here
+  // accept a configFile value or use development by default
+  const file = path.resolve("cypress");
+
+  return getConfigurationByFile(file);
 };
